644b511ae8c2f7de03b843dfa505024afec07d7b
Titoputraaa <titobamulo13@gmail.com>
2025-01-08 00:01:46 +0700

    fakk pusing

--- Main.java
+++ Main.java
@@ -7,8 +7,6 @@ public class Main {
     static String[] items;
     static int[] price;
     static int[] quantitiy;
-    static int result;
-    static double discon;
     
     public static void main(String[] args) {
         do {
@@ -24,20 +22,19 @@ public class Main {
                 case 1:
                     input();
                         break;
-                case 3:
-                    detail();
-                        break;
                 case 2:
-                    count();
+                    int total = count();
                     show();
+                    System.out.println("\nTOTAL : " + total);
                         break;
-
+                case 3:
+                    
+                    break;
                 case 4:
                     System.out.println("===THANKS YOU===");
-                    return;
-            
+                        return;
                 default:
-                System.out.println("input the right choose");
+                    System.out.println("pleas choise the right numbre");
                     break;
             }
         } while (true);
@@ -64,8 +61,9 @@ public class Main {
         }
     }
 
-    static void count() {
+    static int count() {
         int[] priceItems = new int[items.length];
+        int result = 0;
         for (int sum = 0; sum < priceItems.length; sum++) {
             for (int i = 0; i < items.length; i++) {
                 priceItems[i] = price[i] * quantitiy[i];
@@ -75,27 +73,18 @@ public class Main {
         for (int i = 0; i < priceItems.length; i++) {
             result += priceItems[i];
         }
-        System.out.println("\nTOTAL : " + (result - discon));
-    }
-
-    static void detail() {
-        for (int i = 0; i < items.length; i++) {
-            System.out.println(items[i] + "    " + price[i] + "    " + quantitiy[i] + " : " + (price[i] * quantitiy[i]));
-        }
 
         if (result >= 200) {
-            discon = result * 0.05;
+            double discon = result * 0.05;
             result -= discon;
-            System.out.println("discount : " + discon);
         } else if (result >= 100 && result < 200) {
             double discon = result * 0.03;
             result -= discon;
-            System.out.println("discount : " + discon);
         } else {
             System.out.println("total price discount not found");
         }
 
-        System.out.println("TOTAL : " + (result-discon));
+        return result;
     }
 
 
